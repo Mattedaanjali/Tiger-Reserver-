b60aabaf3121e9f5b023abf520767982fe591b2f
Matteda. Anjali <2503b09907@sru.edu.in>
2026-07-12 20:33:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const tigerReserves = [
+{
+    name : "amarbad Tiger Reserve",
+    State : "Telangana",
+    Area : "2611 sq km",
+    founded : "1989",
+    image : "images/amarbad.jpg"
+},
+
+{
+    name : "bandipure Tiger Reserve",
+    State : "Karnataka", 
+    Area :"1454 sq km",
+    founded : "1973",
+    image: "images/bandipure.jpg"
+},
+
+{
+    name : "corbett Tiger Reserve",
+    State :"Uttarakhand",
+    Area :"1318 sq km",
+    founded : "1963",
+    image : "images/corbett.jpg"
+}
+
+];
